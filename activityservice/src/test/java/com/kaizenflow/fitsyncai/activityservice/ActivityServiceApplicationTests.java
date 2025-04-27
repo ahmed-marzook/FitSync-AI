afa60f0c80e8b944e097c3a0da6f044ddc9a50ff
Ahmed Marzook <m4marzook@gmail.com>
2025-04-27 22:38:02 +0100

    Initialize Activity Service with Gradle setup, application configuration, and basic test structure

---
+++ activityservice/src/test/java/com/kaizenflow/fitsyncai/activityservice/ActivityServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kaizenflow.fitsyncai.activityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityServiceApplicationTests {
+
+        @Test
+        void contextLoads() {}
+}
