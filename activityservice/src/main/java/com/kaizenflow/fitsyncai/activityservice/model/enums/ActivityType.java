474da26b45bd130fc5fc46d406a0dd0ac5175647
Ahmed Marzook <m4marzook@gmail.com>
2025-04-27 23:18:02 +0100

    Add Activity model and ActivityType enum; configure MongoDB connection in application.yml; update docker-compose paths for initialization scripts

---
+++ activityservice/src/main/java/com/kaizenflow/fitsyncai/activityservice/model/enums/ActivityType.java
@@ -0,0 +1,12 @@
+package com.kaizenflow.fitsyncai.activityservice.model.enums;
+
+public enum ActivityType {
+        RUNNING,
+        WALKING,
+        CYCLING,
+        SWIMMING,
+        WEIGHT_TRAINING,
+        YOGA,
+        HIIT_TRAINING,
+        MOBILTITY_TRAINING
+}
