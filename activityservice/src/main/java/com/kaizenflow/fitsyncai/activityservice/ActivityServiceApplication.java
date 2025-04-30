dedee49164f7858708a3ee67b5b42805c12fd04c
Ahmed Marzook <m4marzook@gmail.com>
2025-04-30 13:20:18 +0100

    Add Spring Cloud Bootstrap support and enable discovery client for activity, AI, and user services; update configuration files for centralized management

--- activityservice/src/main/java/com/kaizenflow/fitsyncai/activityservice/ActivityServiceApplication.java
+++ activityservice/src/main/java/com/kaizenflow/fitsyncai/activityservice/ActivityServiceApplication.java
@@ -2,10 +2,12 @@ package com.kaizenflow.fitsyncai.activityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableDiscoveryClient
 public class ActivityServiceApplication {
 
         public static void main(String[] args) {
